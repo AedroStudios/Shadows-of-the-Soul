1ecd75d4f52fa0a01ea4e39526b759924f5e5d58
Jose Guevara <94gjose@gmail.com>
2022-06-11 10:29:52 -0400

    add condition to detect when stealth mode is pressed

---
+++ Assets/Scripts/Player/Conditions/IsStealthPressedSO.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using FSM;
+using FSM.ScriptableObjects;
+using Input;
+
+[CreateAssetMenu(fileName = "IsStealthPressed", menuName = "Player/Conditions/Is Stealth Pressed")]
+public class IsStealthPressedSO : StateConditionSO<IsStealthPressed>
+{
+  public InputReader inputReader;
+}
+
+public class IsStealthPressed : Condition
+{
+  protected new IsStealthPressedSO OriginSO => (IsStealthPressedSO)base.OriginSO;
+  InputReader Input { get => OriginSO.inputReader; }
+  bool isStealthPressed;
+
+  public override void Awake(StateMachine stateMachine)
+	{
+    Input.StealthEvent += () => { isStealthPressed = true; };
+	}
+	
+	protected override bool Statement()
+	{
+    if (isStealthPressed)
+    {
+      isStealthPressed = false; // Clean cache
+      return true;
+    }
+
+    return false;
+	}
+}
