f61ca2d2176c1a4ec8008a45f000fced534b9935
Jose Guevara <94gjose@gmail.com>
2022-06-16 14:53:18 -0400

    cache EnergyPerson component

--- Assets/Scripts/NPCs/NPC.cs
+++ Assets/Scripts/NPCs/NPC.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using Energy;
+using System;
 
 namespace NPCs 
 {
   public class NPC : MonoBehaviour
   {
     private float _timer;
+    private EnergyPerson _energyPerson;
 
     private void Awake()
     {
       _timer = 6;
+    }
+
+    private void Start()
+    {
+      _energyPerson = GetComponent<EnergyPerson>();
     }
 
     private void Update() // TESTING
@@ -18,7 +25,7 @@ namespace NPCs
       if (_timer < 0)
       {
         _timer = 6;
-        GetComponent<EnergyPerson>().GiveEnergyToClosestPerson();
+        _energyPerson.GiveEnergyToClosestPerson();
       }
     }
   }
